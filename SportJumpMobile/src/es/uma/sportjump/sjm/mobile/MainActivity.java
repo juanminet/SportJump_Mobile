9539422b59d311c99ff6fc36abc50394fc0632b1
Juan Miguel Muñoz <jmmunoz@paradigmatecnologico.com>
2014-03-03 02:47:52 +0100

    Roboguice configure

--- SportJumpMobile/src/es/uma/sportjump/sjm/mobile/MainActivity.java
+++ SportJumpMobile/src/es/uma/sportjump/sjm/mobile/MainActivity.java
@@ -1,16 +1,39 @@
 package es.uma.sportjump.sjm.mobile;
 
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends Activity {
+import com.google.inject.Inject;
 
+import es.uma.sportjump.sjm.service.FooService;
+
+@ContentView(R.layout.activity_main)
+public class MainActivity extends RoboActivity {
+
+	@InjectView(R.id.image)
+	ImageView image;
+	
+	@Inject
+	FooService fooService;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
 		getActionBar().setDisplayShowTitleEnabled(false);
+		
+		image.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				fooService.brushTeeth();
+				fooService.say(fooService.punch());
+			}
+		});
 	}
 
 	@Override
